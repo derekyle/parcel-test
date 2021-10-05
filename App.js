2ac508cf6b8830b3f8f9bfb57c3b5c4dddf02693
Derek Carr <derek@bscpro.com>
2021-10-05 16:25:38 -0700

    initial files

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Loader from 'rsuite/Loader';
+  
+var mountNode = document.getElementById("app");
+ReactDOM.render(<Loader />, mountNode);
